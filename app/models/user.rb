23eb3c547183da659b9f8297a3ebce32560365cd
ajloutit <ajloutit@gmail.com>
2021-03-30 02:52:42 +0000

    created ability to like and comment. likes verified by server request. both requiring acc login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 
     validates :email, :username, uniqueness: true
     validates :email, :avatar_url, :username, :password, presence: true
+    has_many :posts
+    has_many :comments
+    has_many :likes
 end
